27a24b704318ff8e686232bfb24abaccf6f1053b
killkli <dchensterebay@gmail.com>
2022-07-28 14:14:47 +0800

    deploy

---
+++ precache-manifest.d3968b7883a34487a2d8079f5b650d61.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f964ef27e1fc0c379dd983761d7ab1f7",
+    "url": "/BoyoENG_Test/.htaccess"
+  },
+  {
+    "revision": "f964ef27e1fc0c379dd983761d7ab1f7",
+    "url": "/BoyoENG_Test/.htaccess_AMS.txt"
+  },
+  {
+    "revision": "bdcc71b38edb7404285682ae705b9417",
+    "url": "/BoyoENG_Test/.htaccess_DO.txt"
+  },
+  {
+    "revision": "76eb9e33065ad245080d",
+    "url": "/BoyoENG_Test/css/app.5dd974ab.css"
+  },
+  {
+    "revision": "f70ec3a909f705271831",
+    "url": "/BoyoENG_Test/css/chunk-vendors.facf7b87.css"
+  },
+  {
+    "revision": "adfdcdea5c03ac9426f474cc1ef50d4e",
+    "url": "/BoyoENG_Test/favicon.ico"
+  },
+  {
+    "revision": "7d9421045bcabc1e90e1458e72c6a5cb",
+    "url": "/BoyoENG_Test/icon.png"
+  },
+  {
+    "revision": "985ba0faefe64ca3bb0c665fb4563a8d",
+    "url": "/BoyoENG_Test/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "88280466f33992b17dce5305261e018e",
+    "url": "/BoyoENG_Test/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "f6e728eee5b0a926a91db5eb27800978",
+    "url": "/BoyoENG_Test/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2d81fe480aa9fc0f6067bc0f94670452",
+    "url": "/BoyoENG_Test/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "b7647d12a3f99b3c079c4d9bd53593d8",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "3b9c26616da70b1555dd0893abdc38b9",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "a341f52a7499f028c39bdef9e1fd0353",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "d67e842e7928a3f3d59a80da39deb762",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "08a23740ef986f79ff9049514b1ae2cc",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "acd013ff94f3589134558761fb581e98",
+    "url": "/BoyoENG_Test/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "fcc448503465c5e06a1a874c614d1909",
+    "url": "/BoyoENG_Test/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "769f97066e6c428cb57a77657dacc22d",
+    "url": "/BoyoENG_Test/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "a2ab0d8cca40f4f86f3f4ec393c3507b",
+    "url": "/BoyoENG_Test/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "6a9d3acd69106db00ca794a286e0602a",
+    "url": "/BoyoENG_Test/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "e9c7d5cbf5e30860c62675dceff43ba2",
+    "url": "/BoyoENG_Test/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "5db211040c08cf121ab7ad9c7f1b5032",
+    "url": "/BoyoENG_Test/index.html"
+  },
+  {
+    "revision": "76eb9e33065ad245080d",
+    "url": "/BoyoENG_Test/js/app.fcaf12f3.js"
+  },
+  {
+    "revision": "8c1771d04766518f2ed544fe0d094066",
+    "url": "/BoyoENG_Test/js/calcLevelWorker.2fbbcdb6.worker.js"
+  },
+  {
+    "revision": "f70ec3a909f705271831",
+    "url": "/BoyoENG_Test/js/chunk-vendors.705bfc32.js"
+  },
+  {
+    "revision": "79e9035f921b51079217a9d2fbf49cbd",
+    "url": "/BoyoENG_Test/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/BoyoENG_Test/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/BoyoENG_Test/precache-manifest.d3968b7883a34487a2d8079f5b650d61.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "boyo_vocabulary"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
